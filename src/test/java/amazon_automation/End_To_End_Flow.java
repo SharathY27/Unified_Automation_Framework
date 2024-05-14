09662387acf63a3ead8ab9b53c24874bbb846917
Sharath <Sharath@DESKTOP-I3V29VC>
2024-05-14 22:09:37 +0530

    update

--- src/test/java/amazon_automation/End_To_End_Flow.java
+++ src/test/java/amazon_automation/End_To_End_Flow.java
@@ -14,6 +14,7 @@ public class End_To_End_Flow extends BaseTestForParallelExecution {
 	public static void selecting_product() {
 		try {
 			Amazon_HomePage amazon_HomePage = new Amazon_HomePage();
+			Thread.sleep(3000);
 			amazon_HomePage.productSearchBox.sendKeys("water bottles");
 			amazon_HomePage.searchButton.click();
 			Thread.sleep(2000);
